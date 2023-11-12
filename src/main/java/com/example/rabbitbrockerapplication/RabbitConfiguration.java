a054875e9e6a2599b1e2d5293eb21fb184d1ace5
Illia Belikau <ilasemcenkov1@gmail.com>
2023-11-11 23:02:38 -1100

    first commit

---
+++ src/main/java/com/example/rabbitbrockerapplication/RabbitConfiguration.java
@@ -0,0 +1,51 @@
+package com.example.rabbitbrockerapplication;
+
+import org.slf4j.Logger;
+import org.springframework.amqp.core.Queue;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfiguration {
+
+    Logger logger = LoggerFactory.getLogger(RabbitConfiguration.class);
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        return new CachingConnectionFactory("localhost");
+    }
+
+    @Bean
+    public AmqpAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        return new RabbitTemplate(connectionFactory());
+    }
+
+    @Bean
+    public Queue myQueue() {
+        return new Queue("myQueue");
+    }
+
+    @Bean
+    public SimpleMessageListenerContainer messageListenerContainer() {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        //Здесь мы указываем название очередей на которые подписываемся
+        container.setConnectionFactory(connectionFactory());
+        container.setQueueNames("myQueue");
+        //Также что нам нужно сделать при получении сообщения, в данном случае мы прописываеи logger
+        container.setMessageListener(message -> logger.info("Received from myQueue : " + new String(message.getBody())));
+        return container;
+    }
+
+}
---
+++ src/test/java/com/example/rabbitbrockerapplication/RabbitBrockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitbrockerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitBrockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
