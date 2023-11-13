7b4b573aebf56e1bf5e032d710969ad9311cc06b
Illia Belikau <ilasemcenkov1@gmail.com>
2023-11-12 19:31:19 -1100

    direct topic fanout

--- src/main/java/com/example/rabbitbrockerapplication/RabbitConfiguration.java
+++ src/main/java/com/example/rabbitbrockerapplication/RabbitConfiguration.java
@@ -1,9 +1,8 @@
 package com.example.rabbitbrockerapplication;
 
 import org.slf4j.Logger;
-import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.*;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
@@ -33,19 +32,66 @@ public class RabbitConfiguration {
     }
 
     @Bean
-    public Queue myQueue() {
-        return new Queue("myQueue");
+    public Queue myQueue1() {
+        return new Queue("myQueue1");
     }
 
     @Bean
-    public SimpleMessageListenerContainer messageListenerContainer() {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        //Здесь мы указываем название очередей на которые подписываемся
-        container.setConnectionFactory(connectionFactory());
-        container.setQueueNames("myQueue");
-        //Также что нам нужно сделать при получении сообщения, в данном случае мы прописываеи logger
-        container.setMessageListener(message -> logger.info("Received from myQueue : " + new String(message.getBody())));
-        return container;
+    public Queue myQueue2() {
+        return new Queue("myQueue2");
     }
+
+    public FanoutExchange fanoutExchange() {
+        return new FanoutExchange("common-exchange");
+    }
+
+    public DirectExchange directExchange() {
+        return new DirectExchange("direct-exchange");
+    }
+    @Bean
+    public Binding binding1() {
+        return BindingBuilder.bind(myQueue1()).to(directExchange()).with("error");
+    }
+
+    @Bean
+    public Binding binding2() {
+        return BindingBuilder.bind(myQueue2()).to(directExchange()).with("warning");
+    }
+
+    @Bean
+    public Binding binding3() {
+        return BindingBuilder.bind(myQueue2()).to(directExchange()).with("info");
+    }
+
+    @Bean
+    public TopicExchange topicExchange() {
+        return new TopicExchange("topic-exchange");
+    }
+
+    @Bean
+    public Binding binding11() {
+        return BindingBuilder.bind(myQueue1()).to(topicExchange()).with("one.*");
+    }
+
+    @Bean
+    public Binding binding12() {
+        return BindingBuilder.bind(myQueue2()).to(topicExchange()).with("*.second");
+    }
+
+
+
+
+
+
+//    @Bean
+//    public SimpleMessageListenerContainer messageListenerContainer() {
+//        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+//        //Здесь мы указываем название очередей на которые подписываемся
+//        container.setConnectionFactory(connectionFactory());
+//        container.setQueueNames("myQueue");
+//        //Также что нам нужно сделать при получении сообщения, в данном случае мы прописываеи logger
+//        container.setMessageListener(message -> logger.info("Received from myQueue : " + new String(message.getBody())));
+//        return container;
+//    }
 
 }
--- src/main/java/com/example/rabbitbrockerapplication/SampleController.java
+++ src/main/java/com/example/rabbitbrockerapplication/SampleController.java
@@ -3,27 +3,33 @@ package com.example.rabbitbrockerapplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @RestController
 public class SampleController {
 
     Logger logger = LoggerFactory.getLogger(SampleController.class);
 
-    private final AmqpTemplate template;
+    private final RabbitTemplate template;
 
     @Autowired
-    public SampleController(AmqpTemplate template) {
+    public SampleController(RabbitTemplate template) {
         this.template = template;
     }
     @PostMapping("/emit")
     public ResponseEntity<String> emit(@RequestBody String message) {
         logger.info("Emit to myQueue");
+        template.setExchange("common-exchange");
+        template.setExchange("topic-exchange");
         template.convertAndSend("myQueue", message);
         return ResponseEntity.ok("Success emit to queue");
     }
+
 }
